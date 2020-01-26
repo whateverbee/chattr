6dfc10aaba4b7a0afa79484eceadd0cdbdbda2a2
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-01-26 16:46:39 -0500

    adds message model and association

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Jane", password: "12345")
+User.create(username: "Bob", password: "password")
+User.create(username: "Sam", password: "anotherpassword")
+User.create(username: "Jonas", password: "111222y")
+User.create(username: "Rashid", password: "asgagsdew2")
+Message.new(body: "First message ever! Super cool", user_id: 3)
+Message.new(body: "Oh hey, sup?", user_id: 5)
+Message.new(body: "Hello!", user_id: 1)
