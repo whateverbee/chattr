fd21767836f7574f6bf7a501ebe64901ad0e3708
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-01-26 17:21:39 -0500

    begin authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id 
+      flash[:success] = "You have logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Something went wrong with your login info"
+      render 'new'
+    end
+  end
+
+  def destroy 
+    session[:user_id] = nil 
+    flash[:success] = "You have logged out"
+    redirect_to login_path
+  end
+  
 end
