621691cccaab27a80581cc46eb003d875ea2f535
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-01-26 17:30:17 -0500

    adds flash partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       flash[:success] = "You have logged in"
       redirect_to root_path
     else
-      flash.now[:error] = "Something went wrong with your login info"
+      flash[:error] = "Something went wrong with your login info"
       render 'new'
     end
   end
