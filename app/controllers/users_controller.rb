4890a01311f457276a79d5bdfb735237bc25eceb
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-01-28 17:01:50 -0500

    troubleshooting signup redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save 
       flash[:success] = "Welcome to chattr, #{@user.username}"
+      session[:user_id] = @user.id 
       redirect_to root_path
     else 
       render 'new'
