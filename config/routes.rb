9fc8273a06fadfa31b8f6495b88eb445c86bedf9
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-01-28 16:47:22 -0500

    adds user registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
   mount ActionCable.server, at: '/cable'
